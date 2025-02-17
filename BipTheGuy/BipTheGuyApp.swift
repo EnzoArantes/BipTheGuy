45fc3ef655836da0577fc6b051ebee4b19a8f6f3
Zo-hack <enzoarantes2000@gmail.com>
2025-02-17 11:49:53 -0500

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Enzo Arantes on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
